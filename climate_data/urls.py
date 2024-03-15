52f8272389beaec5abd4e38313eabef42901f665
heCarlo <carlosgfkp@outlook.com>
2024-03-14 22:26:18 -0300

    feat: modified urls files

--- climate_data/urls.py
+++ climate_data/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path, include
-from ..climate_data.view import (
-    ForecastWeatherDataEntityListView,
-    ForecastWeatherDataEntityDetailView,
-    create_forecast,
-    update_forecast,
-    delete_forecast,
-)
+from .view.ForecastWeatherDataView import ForecastWeatherDataEntityViewSet
 
 urlpatterns = [
-    path('forecast/', ForecastWeatherDataEntityListView.as_view(), name='forecast-list'),
-    path('forecast/<uuid:pk>/', ForecastWeatherDataEntityDetailView.as_view(), name='forecast-detail'),
-    path('forecast/create/', create_forecast, name='forecast-create'),
-    path('forecast/update/<uuid:pk>/', update_forecast, name='forecast-update'),
-    path('forecast/delete/<uuid:pk>/', delete_forecast, name='forecast-delete'),
+    path('forecast/', ForecastWeatherDataEntityViewSet.as_view({'get': 'list', 'post': 'create'}), name='forecast-list'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from ..climate_data.urls import urlpatterns
 
 urlpatterns = [
     path('forestWeatherData/', include('climate_data.urls')),
